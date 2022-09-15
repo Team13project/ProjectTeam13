4e264cfc9c9427706991e86a42cb027bd18edcc4
Danis7011 <113059414+Danis7011@users.noreply.github.com>
2022-09-15 21:01:17 +0200

    Update Covid19.vb

--- Covid19.vb
+++ Covid19.vb
@@ -2,6 +2,31 @@
 Option Explicit On
 Option Infer Off
 Option Strict On
+
 Public Class Covid19
+    Inherits Disease
+    Public Sub New(ninfect As Integer)
+        MyBase.New(ninfect)
+    End Sub
+    Public Sub New()
+
+    End Sub
+
+    'Methods
+    'extending function for symptoms
+    Public Overrides Function symptoms() As String
+        Dim v As String
+        v = CStr("4. Tiredness" & vbNewLine & "5. Loss of taste or smell")
+        Return CStr(MsgBox(MyBase.symptoms() & vbNewLine & v))
+
+    End Function
+
+    'function for treatment info
+    Public Overrides Function treatment() As String
+        Dim v As String
+        v = CStr("1) Nirmatrelvir with Ritonavi (Tablet)" & vbNewLine & "2) Remdesivir(infusion)" & vbNewLine & "3) Bebtelovimab Monoclonal antibody(Injection)" & vbNewLine & "SOME PREVENTIONS:" & vbNewLine & vbNewLine & "1. Wear medical masks" & vbNewLine & "2. Take vaccines" & vbNewLine & "3. Social distance")
+        Return CStr(MsgBox(vbNewLine & v))
+    End Function
 
 End Class
+
