39235569b4c5a798b69348304cb6b76b4ed53a37
Moreitu1985 <113109028+Moreitu1985@users.noreply.github.com>
2022-09-15 16:01:27 +0200

    Update HIV,AIDS.vb

--- HIV,AIDS.vb
+++ HIV,AIDS.vb
@@ -4,4 +4,29 @@ Option Infer Off
 Option Strict On
 Public Class HIV_AIDS
 
+    'Constructor
+    Public Sub New(ninfect As Integer)
+        MyBase.New(ninfect)
+    End Sub
+    Public Sub New()
+
+    End Sub
+
+    'Methods
+    'extending function for symptoms
+    Public Overrides Function symptoms() As String
+        Dim v As String
+        v = CStr("4. Joint pains" & vbNewLine & "5. Headache/Muscle aches" & vbNewLine & "6. Weight loss" & vbNewLine & "7. Diarrhea.")
+        Return CStr(MsgBox(MyBase.symptoms() & vbNewLine & v))
+
+    End Function
+
+    'function for treatment info
+    Public Overrides Function treatment() As String
+        Dim v As String
+        v = CStr("1) Antiretroviral Therapy" & vbNewLine & vbNewLine & "SOME PREVENTIONS:" & vbNewLine & "1) Use contraceptives" & vbNewLine & "2) Do not share needles")
+
+        Return CStr(MsgBox(vbNewLine & v))
+
+    End Function
 End Class
